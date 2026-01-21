b688a8b5906a954f72102119f06408b857ab2ddf
imtomasz <thomas.brs@outlook.fr>
2026-01-21 16:56:31 +0100

    Mise à jour bdd

--- contacts.sql
+++ contacts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : mysql
--- Généré le : mar. 20 jan. 2026 à 14:59
+-- Généré le : mer. 21 jan. 2026 à 15:55
 -- Version du serveur : 8.0.44
 -- Version de PHP : 8.3.26
 
@@ -37,6 +37,18 @@ CREATE TABLE `contact` (
   `id_groupe` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id_contact`, `nom`, `prenom`, `dateNaissance`, `telephone`, `email`, `id_groupe`) VALUES
+(1, 'Kelevra', 'Slevin', '1985-03-20', 625256617, 'chienmechant@gmail.com', 1),
+(8, 'Landa', 'Hans', '1956-10-04', 637919036, 'hans.landa@iiireich.de', 2),
+(9, 'Le', 'Loup', '0006-06-06', 666666666, 'lamort@afpa.fr', 2),
+(10, 'Annatar', 'Sauron', '0001-06-03', 667513687, 'seigneurtenebreuxdu34@mordor.eng', 2),
+(11, 'Unchained', 'Django', '1783-12-13', 678469375, 'fauteurdetrouble@metis.fr', 1),
+(12, 'Gamgee', 'Sam', '0142-06-14', 689566738, 'levraihero@comte.fr', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +59,26 @@ CREATE TABLE `contact_interet` (
   `id_contact` int NOT NULL,
   `id_interet` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `contact_interet`
+--
+
+INSERT INTO `contact_interet` (`id_contact`, `id_interet`) VALUES
+(10, 1),
+(8, 2),
+(10, 2),
+(1, 3),
+(10, 3),
+(11, 3),
+(12, 3),
+(1, 4),
+(11, 4),
+(8, 5),
+(9, 6),
+(11, 9),
+(12, 9),
+(1, 10);
 
 -- --------------------------------------------------------
 
@@ -59,6 +91,14 @@ CREATE TABLE `groupe` (
   `nomGroupe` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id_groupe`, `nomGroupe`) VALUES
+(1, 'Gentils'),
+(2, 'Méchants');
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +108,30 @@ CREATE TABLE `groupe` (
 CREATE TABLE `interet` (
   `id_interet` int NOT NULL,
   `nomInteret` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `interet`
+--
+
+INSERT INTO `interet` (`id_interet`, `nomInteret`) VALUES
+(1, 'Créer des anneaux'),
+(2, 'Imposer une dictature'),
+(3, 'Tuer ses ennemis'),
+(4, 'Se venger'),
+(5, 'Apprendre des langues'),
+(6, 'Récupérer son dû'),
+(9, 'Se battre pour la liberté'),
+(10, 'Faire des blagues auxquelles personne ne rigole');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `test`
+--
+
+CREATE TABLE `test` (
+  `TEST` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -114,6 +178,12 @@ ALTER TABLE `interet`
   ADD PRIMARY KEY (`id_interet`);
 
 --
+-- Index pour la table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`TEST`);
+
+--
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
@@ -129,19 +199,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id_contact` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_contact` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `groupe`
 --
 ALTER TABLE `groupe`
-  MODIFY `id_groupe` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_groupe` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `interet`
 --
 ALTER TABLE `interet`
-  MODIFY `id_interet` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_interet` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `test`
+--
+ALTER TABLE `test`
+  MODIFY `TEST` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `user`
